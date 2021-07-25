c6c84ef9d313b4217849cbd99e44634715a59732
dmitriy.lysenko <dmitriy.lysenko@gs-labs.ru>
2021-07-25 23:59:51 +0300

    add some changes

--- linux.py
+++ linux.py
@@ -1,11 +1,15 @@
 from subprocess import run
 from datetime import datetime
 
+
 def run_console(command, key):
     return run([command, key], capture_output=True).stdout
 
+
 stdout = run_console('ps', '-aux').decode()
 
+with open('ps_out_zeroes-1801-5ac30b', 'r') as stdout:
+    read = stdout.read()
 
 def table_(stdout_data: str):
     """Парсин все таблицы и предоставление результата в как строка = список"""
@@ -21,7 +25,9 @@ def table_(stdout_data: str):
     else:
         return list_parsed[1:-1]
 
-parsed_table = table_(stdout)
+
+parsed_table = table_(read)
+
 
 def users(parsed_table):
     """подсчёт юзеров из списка по нулевому индексу"""
@@ -30,11 +36,12 @@ def users(parsed_table):
         list_of_users.append(i[0])
     return set(list_of_users)
 
-users = users(parsed_table)
 
+users = users(parsed_table)
 
 print(f'Пользователи системы: {users}, \n'
       f'Процессов запущено: {len(parsed_table)}')
+
 
 def count_process_by_users():
     """подсчёт количества процессов из списка по нулевому индексу"""
@@ -45,7 +52,6 @@ def count_process_by_users():
 
 
 def memory_mb():
-
     memory_precent = 0
     process = 0.0
     name_process = ''
@@ -54,7 +60,7 @@ def memory_mb():
         if float(i[3]) > process:
             process = float(i[3])
             name_process = i[10][:20]
-    return round((memory_precent / 100) * 16000, 1), name_process
+    return round(memory_precent, 1), name_process
 
 
 def cpu():
@@ -69,24 +75,28 @@ def cpu():
     return round(cpu_precent, 1), name_process
 
 
-
-print(parsed_table)
-
-
-print(f'Всего памяти используется: {memory_mb()[0]} mb\n\r'
-      f'Больше всего памяти использует: {memory_mb()[1]}\n\r'
-      f'Всего CPU используется: {cpu()[0]}%\n\r'
-      f'Больше всего CPU использует: {cpu()[1]}\n\r')
-
-
+print(f'Всего памяти используется: {memory_mb()[0]}% от общего объёма')
+if memory_mb()[0] != 0.0:
+    print(f'Больше всего памяти использует: {memory_mb()[1]}')
+else:
+    print(f'память не используется или очень мало используется')
+print(f'Всего CPU используется: {cpu()[0]}%')
+if cpu()[0] != 0:
+    print(f'Больше всего CPU использует: {cpu()[1]}')
+else:
+    print(f'CPU не используется')
 
 with open(f'{datetime.now()}-scan.txt', 'w') as scan:
     scan.write(
-                f'Пользователи системы: {users}, \n\r'
-                f'Процессов запущено: {len(parsed_table)}\n\r'
-                f'Всего памяти используется: {memory_mb()[0]} mb\n\r'
-                f'Больше всего памяти использует: {memory_mb()[1]}\n\r'
-                f'Всего CPU используется: {cpu()[0]}%\n\r'
-                f'Больше всего CPU использует: {cpu()[1]}\n\r'
-    )
-
+        f'Пользователи системы: {users}, \n'
+        f'Процессов запущено: {len(parsed_table)}\n'
+        f'Всего памяти используется: {memory_mb()[0]}% от общего объёма\n')
+    if memory_mb()[0] != 0.0:
+        scan.write(f'Больше всего памяти использует: {memory_mb()[1]}\n')
+    else:
+        scan.write(f'память не используется или очень мало используется\n')
+    scan.write(f'Всего CPU используется: {cpu()[0]}%\n')
+    if cpu()[0] != 0:
+        scan.write(f'Больше всего CPU использует: {cpu()[1]}\n')
+    else:
+        scan.write(f'CPU не используется\n')
